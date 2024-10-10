7959ab487e64a4650a444cca7364e2e188755895
Jovis <150299519+mmm-sauce@users.noreply.github.com>
2024-10-10 12:33:45 -0700

    Tweeks

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
 
-var movespeed = 350
+var movespeed = 500
 var current_speed = 0
-const accel = 15  # acceleration rate
-const decel = 0.03  # deceleration rate when no input
+const accel = 500  # acceleration rate
+const decel = 0.1  # deceleration rate when no input
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -12,10 +12,10 @@ func _process(delta):
 		global.top_briefcase = self
 	
 	if Input.is_action_pressed("left"):
-		current_speed = max((current_speed) - 40*accel * delta, -movespeed)
+		current_speed = max((current_speed) - 40*accel * delta*delta, -movespeed)
 		$Sprite2D.flip_h = true
 	elif Input.is_action_pressed("right"):
-		current_speed = min((current_speed) + 40*accel * delta, movespeed)
+		current_speed = min((current_speed) + 40*accel * delta*delta, movespeed)
 		$Sprite2D.flip_h = false
 	else:
 		current_speed = lerp(float(current_speed), 0.0, float(decel))
